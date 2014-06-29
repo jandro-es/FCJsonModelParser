d3e2502f6944bf73e16cfe8ede73495784fafd6a
Alejandro Barros Cuetos <jandrob1978@gmail.com>
2014-06-30 01:30:50 +0200

    Initial Commit

---
+++ FCJsonModelParser/FCJsonModelParser.h
@@ -0,0 +1,19 @@
+//
+//  FCJsonModelParser.h
+//  FCJsonModelParser
+//
+//  Created by Alejandro Barros Cuetos on 30/06/14.
+//  Copyright (c) 2014 Alejandro Barros Cuetos. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FCJsonModelParser.
+FOUNDATION_EXPORT double FCJsonModelParserVersionNumber;
+
+//! Project version string for FCJsonModelParser.
+FOUNDATION_EXPORT const unsigned char FCJsonModelParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FCJsonModelParser/PublicHeader.h>
+
+
